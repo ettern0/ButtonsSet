176380e93b59ea58582ec6d50e56ae3c6d816dff
ettern0 <evserdyukov@gmail.com>
2022-05-13 19:07:52 +0200

    Initial Commit

---
+++ ButtonsSet/ButtonsSetApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonsSetApp.swift
+//  ButtonsSet
+//
+//  Created by Evgeny Serdyukov on 13.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct ButtonsSetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
