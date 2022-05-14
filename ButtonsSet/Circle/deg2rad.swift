bb93684b06c5f2fbb44ed40f811ad1a8cb338d5c
ettern0 <evserdyukov@gmail.com>
2022-05-14 06:05:21 +0200

    Implement func to calculate rad from degree

---
+++ ButtonsSet/Circle/deg2rad.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  ButtonsSet
+//
+//  Created by Evgeny Serdyukov on 14.05.2022.
+//
+
+import Foundation
+
+func deg2rad(_ number: Double) -> Double {
+    return number * .pi / 180
+}
