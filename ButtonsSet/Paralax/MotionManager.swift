b50e369505e4d37254e641a3af7ac7ec78632e46
ettern0 <evserdyukov@gmail.com>
2022-05-14 03:27:04 +0200

    Implement motion manager

--- ButtonsSet/Paralax/MotionManager.swift
+++ ButtonsSet/Paralax/MotionManager.swift
@@ -5,4 +5,29 @@
 //  Created by Evgeny Serdyukov on 14.05.2022.
 //
 
-import Foundation
+import CoreMotion
+
+class MotionManager: ObservableObject {
+
+    @Published var pitch: Double = 0.0
+    @Published var roll: Double = 0.0
+
+    private var manager: CMMotionManager
+
+    init() {
+        self.manager = CMMotionManager()
+        self.manager.deviceMotionUpdateInterval = 1/60
+        self.manager.startDeviceMotionUpdates(to: .main) { (motionData, error) in
+            guard error == nil else {
+                print(error!)
+                return
+            }
+
+            if let motionData = motionData {
+                self.pitch = motionData.attitude.pitch
+                self.roll = motionData.attitude.roll
+            }
+        }
+
+    }
+}
