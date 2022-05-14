44e4d85e67542845d5607dfe4ac2b273cbc30fe4
ettern0 <evserdyukov@gmail.com>
2022-05-14 03:27:30 +0200

    Implement paralaax motion modifier

--- ButtonsSet/Paralax/ParalaxMotionModifier.swift
+++ ButtonsSet/Paralax/ParalaxMotionModifier.swift
@@ -5,4 +5,18 @@
 //  Created by Evgeny Serdyukov on 14.05.2022.
 //
 
-import Foundation
+import SwiftUI
+import CoreMotion
+
+struct ParallaxMotionModifier: ViewModifier {
+
+    @ObservedObject var manager: MotionManager
+    var magnitude: Double
+    var additionalOffset: Double = 1
+
+    func body(content: Content) -> some View {
+        content
+            .offset(x: CGFloat(manager.roll * magnitude * additionalOffset), y: CGFloat(manager.pitch * magnitude * additionalOffset))
+    }
+}
+
